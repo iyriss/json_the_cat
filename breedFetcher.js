3edff39a57f00daa9ea2fc9f87523351768f52cd
Iyris Vigil <iyrisvigil@gmail.com>
2020-08-28 00:34:43 +0000

    refactoring breedFetcher to use a callback function from index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,36 @@
 const request = require('request');
-const userInput = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('Oops! There is an error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('No results found for that breed');
+
+
+// request(url, (error, response, body) => {
+//   if (error) {
+//     console.log('Oops! There is an error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   } else {
+//     const data = JSON.parse(body);
+//     if (data.length === 0) {
+//       console.log('No results found for that breed');
+//     } else {
+//       console.log(data[0].description);  //description is fetched via an API request, which is network I/O
+//     }
+//   }
+// });
+
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null, 'No results found for that breed');
+      } else {
+        callback(null, data[0].description);  //description is fetched via an API request, which is network I/O
+      }
     }
-  }
-});
+  });
+}
+
+module.exports = { fetchBreedDescription };
